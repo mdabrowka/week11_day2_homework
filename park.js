45bc54e5028f034ac0ba10a15335b002bb075e1b
Marta <martadabrowka@gmail.com>
2017-12-05 17:08:16 +0000

    passed can add a dino to an enclosure

--- park.js
+++ park.js
@@ -6,5 +6,9 @@ Park.prototype.enclosureSize = function () {
   return this.enclosure.length;
 }
 
+Park.prototype.addToEnclosure = function (dino) {
+  this.enclosure.push(dino);
+}
+
 
 module.exports = Park;
--- specs/park_spec.js
+++ specs/park_spec.js
@@ -1,20 +1,26 @@
 const Park = require('../park.js');
+const Dinosaur = require('../dinosaur.js');
 const assert = require('assert');
 
 describe('Park', function() {
    let park;
+   let dinosaur1;
+   let dinosaur2;
 
 
   beforeEach(function () {
      park = new Park();
+     dinosaur1 = new Dinosaur("Diplodocus", 7);
+     dinosaur2 = new Dinosaur("Styracosaurus", 5);
  })
 
  it('enclosure starts empty', function () {
    assert.strictEqual(park.enclosureSize(), 0);
  });
 
-
-
-
+it('should add a dino to an enclosure', function () {
+  park.addToEnclosure(dinosaur1);
+  assert.strictEqual(park.enclosureSize(), 1);
+})
 
 });
